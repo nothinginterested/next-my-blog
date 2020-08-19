886af61c8bd153a8751544cb371a5a130530124d
你的名字 <>
2020-08-19 14:43:26 +0800

    创建migration

---
+++ src/migration/1597817860490-CreatePost.ts
@@ -0,0 +1,25 @@
+import {MigrationInterface, QueryRunner} from 'typeorm';
+import {Table} from 'typeorm/index';
+
+export class CreatePost1597817860490 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+       return  await queryRunner.createTable(new Table({
+            name: 'posts',
+            columns: [{name: 'id', type: 'int', isPrimary: true, isGenerated: true, generationStrategy: 'increment'},
+                {
+                    name: 'title',
+                    type: 'varchar',
+                }, {
+                    name: 'content',
+                    type: 'text'
+                }]
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        return  await queryRunner.dropTable('posts');
+
+    }
+
+}
