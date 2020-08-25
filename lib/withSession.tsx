dd0c2d9432150c4a72931099b33703d4023f3c7f
你的名字 <>
2020-08-25 15:45:30 +0800

    添加 withSession

---
+++ lib/withSession.tsx
@@ -0,0 +1,10 @@
+import {withIronSession} from 'next-iron-session';
+import {GetServerSideProps, NextApiHandler} from 'next';
+
+export function withSession(handler: NextApiHandler | GetServerSideProps) {
+    return withIronSession(handler, {
+        password: process.env.SECRET,
+        cookieName: 'blog',
+        cookieOptions: {secure: false}
+    });
+}
